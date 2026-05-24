71bd0aee30a9105c7e41ef2147567b7a010e3da8
W-G-M <guptaaswar@gmail.com>
2026-05-24 14:48:36 +0530

    Use environment variable for server port

--- web/server.py
+++ web/server.py
@@ -151,4 +151,5 @@ if __name__ == "__main__":
     print("  ◆ Ctrl+C to stop")
     print("═" * 54 + "\n")
 
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
